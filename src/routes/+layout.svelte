baa92f26853a9ef53b36edabb99c436822f0f2a7
Edwards Presume <edwardspresume@gmail.com>
2023-11-25 16:25:39 -0500

    Add progress bar to layout

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,8 +1,12 @@
 <script>
 	import '../app.pcss';
 
+	import { Bar } from '@bobbymannino/svelte-progress';
+
 	import SiteMainHeader from '$components/siteMainHeader/SiteMainHeader.svelte';
 </script>
+
+<Bar color="#6D28D9" size="big" speed="fast" />
 
 <SiteMainHeader />
 
