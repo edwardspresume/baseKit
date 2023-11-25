e43ce7661f467678b73a8ddb54f872ce00e2f2f0
Edwards Presume <edwardspresume@gmail.com>
2023-11-25 13:41:41 -0500

    docs(navLinks.ts): add JSDoc comments and improve documentation for createNavLink function

--- src/lib/navLinks.ts
+++ src/lib/navLinks.ts
@@ -14,7 +14,16 @@ export type NavLink = {
 	isExternal?: boolean;
 };
 
-// Function to create a new navigation link
+/**
+ * Create a NavLink object for use in navigation menus.
+ *
+ * @param title - The text to display for the navigation link.
+ * @param path - The route path from the Routes enum that this link should navigate to.
+ * @param ariaLabel - (Optional) The aria-label for the link, for accessibility. Defaults to the title if not provided.
+ * @param isExternal - (Optional) Flag to indicate if the link is external to the application's routing context (e.g., linking to an external site).
+ * @returns The NavLink object with the provided properties.
+ * @throws Will throw an error if title or path is not provided.
+ */
 export const createNavLink = (
 	title: string,
 	path: Routes,
