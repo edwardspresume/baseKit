accd171a611520b85ba91c45127dd4cf3110dfb5
Edwards Presume <edwardspresume@gmail.com>
2023-11-25 12:58:43 -0500

    chore(postcss.config.cjs): add tailwindNesting and presetEnv plugins to the PostCSS configuration style(app.pcss): define global styles using CSS variables for font, colors, and background chore(tailwind.config.cjs): extend the Tailwind theme to include custom colors and container settings

--- postcss.config.cjs
+++ postcss.config.cjs
@@ -1,11 +1,23 @@
 const tailwindcss = require('tailwindcss');
 const autoprefixer = require('autoprefixer');
+const tailwindNesting = require('tailwindcss/nesting');
+const presetEnv = require('postcss-preset-env');
 
 const config = {
 	plugins: [
 		//Some plugins, like tailwindcss/nesting, need to run before Tailwind,
+
+		tailwindNesting,
+
 		tailwindcss(),
+
 		//But others, like autoprefixer, need to run after,
+
+		presetEnv({
+			stage: 1,
+			features: { 'nesting-rules': false }
+		}),
+
 		autoprefixer
 	]
 };
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -3,7 +3,28 @@ const config = {
 	content: ['./src/**/*.{html,js,svelte,ts}'],
 
 	theme: {
-		extend: {}
+		container: {
+			center: true,
+			padding: '2rem',
+			screens: {
+				'2xl': '1500px'
+			}
+		},
+		extend: {
+			colors: {
+				background: 'var(--background)',
+				foreground: 'var(--foreground)',
+				primary: {
+					DEFAULT: 'var(--primary-color)'
+				},
+				secondary: {
+					DEFAULT: 'var(--secondary-color)'
+				},
+				tertiary: {
+					DEFAULT: 'var(--tertiary-color)'
+				}
+			}
+		}
 	},
 
 	plugins: []
