0e632ab4a4289618ef6b4e6fa97c4211bdb3bd6c
Edwards Presume <edwardspresume@gmail.com>
2023-11-26 07:25:46 -0500

    Add enhancedImages plugin to vite.config.ts and update dev and preview scripts in package.json

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,9 @@
+import { enhancedImages } from '@sveltejs/enhanced-img';
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
-	plugins: [sveltekit()],
+	plugins: [enhancedImages(), sveltekit()],
 	test: {
 		globals: true,
 		environment: 'jsdom',
