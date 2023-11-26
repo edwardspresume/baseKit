34a68a8e55a5ec4a0cff985572b538c731bd0b4a
Edwards Presume <edwardspresume@gmail.com>
2023-11-26 11:25:06 -0500

    Update test for home page heading

--- tests/test.ts
+++ tests/test.ts
@@ -1,6 +1,6 @@
 import { expect, test } from '@playwright/test';
 
-test('about page has expected h1', async ({ page }) => {
-	await page.goto('/about');
-	await expect(page.getByRole('heading', { name: 'About this app' })).toBeVisible();
+test('home page has expected h1', async ({ page }) => {
+	await page.goto('/');
+	await expect(page.getByRole('heading', { name: 'Home page' })).toBeVisible();
 });
