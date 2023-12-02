2f66b1e454d260dc1fb4595a5d94f704f5afa0a9
Edwards Presume <edwardspresume@gmail.com>
2023-12-02 16:35:41 -0500

    Add $validations alias to svelte.config.js

--- svelte.config.js
+++ svelte.config.js
@@ -15,7 +15,8 @@ const config = {
 
 		alias: {
 			$assets: 'src/lib/assets',
-			$components: 'src/lib/components'
+			$components: 'src/lib/components',
+			$validations: 'src/lib/validations'
 		},
 
 		// remove this if you don't want prerendering
