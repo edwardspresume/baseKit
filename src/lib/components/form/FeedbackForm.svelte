e4d5bc17a75f7861657bec902879bfcaf2fc7bf8
Edwards Presume <edwardspresume@gmail.com>
2023-12-03 17:56:03 -0500

    feat(FeedbackForm.svelte): add FeedbackForm component to handle user feedback feat(FeedbackValidationSchema.ts): add validation schema for feedback form message field

---
+++ src/lib/components/form/FeedbackForm.svelte
@@ -0,0 +1,67 @@
+<script lang="ts">
+	import { page } from '$app/stores';
+
+	import { superForm } from 'sveltekit-superforms/client';
+
+	import { toast } from 'svelte-sonner';
+
+	import { FeedbackValidationSchema } from '$validations/feedbackValidationSchema';
+
+	import { buttonVariants } from '$components/ui/button';
+	import * as Dialog from '$components/ui/dialog';
+	import SubmitButton from './SubmitButton.svelte';
+	import TextArea from './TextArea.svelte';
+
+	const { enhance, form, errors, message, delayed } = superForm($page.data.feedbackForm, {
+		id: 'feedbackForm',
+		resetForm: true,
+		taintedMessage: null,
+		validators: FeedbackValidationSchema,
+
+		onUpdated: () => {
+			if (!$message) return;
+
+			const { alertType, alertText } = $message;
+
+			if (alertType === 'error') {
+				toast.error(alertText);
+			}
+
+			if (alertType === 'success') {
+				toast.success(alertText);
+			}
+		}
+	});
+</script>
+
+<Dialog.Root>
+	<Dialog.Trigger class={buttonVariants({ variant: 'outline' })}>Feedback</Dialog.Trigger>
+	<Dialog.Content class="sm:max-w-[425px]">
+		<Dialog.Header>
+			<Dialog.Title>Feedback</Dialog.Title>
+			<Dialog.Description>
+				Let me know if you experience any issues or have any suggestions to improve this app
+			</Dialog.Description>
+		</Dialog.Header>
+		<form
+			use:enhance
+			method="POST"
+			action="/sendFeedbackToEmail"
+			aria-label="Send feedback"
+			class="grid gap-5"
+		>
+			<TextArea
+				rows="6"
+				name="message"
+				label="Message"
+				placeholder="Enter your message"
+				bind:value={$form.message}
+				errorMessage={$errors.message}
+			/>
+
+			<SubmitButton disabled={$delayed}>
+				{$delayed ? 'Sending...' : 'Send'}
+			</SubmitButton>
+		</form>
+	</Dialog.Content>
+</Dialog.Root>
---
+++ src/lib/validations/feedbackValidationSchema.ts
@@ -0,0 +1,12 @@
+import { z } from 'zod';
+
+const MIN_FEEDBACK_LENGTH = 10;
+
+export const FeedbackValidationSchema = z.object({
+	message: z
+		.string()
+		.min(MIN_FEEDBACK_LENGTH, `Message must be at least ${MIN_FEEDBACK_LENGTH} characters`)
+		.transform((str) => str.trim())
+});
+
+export type FeedbackValidationSchemaType = typeof FeedbackValidationSchema;
