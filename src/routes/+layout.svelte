90b94bdb4ed708a9ecf27d2e8a1dcef6591a9da1
Edwards Presume <edwardspresume@gmail.com>
2023-12-03 17:45:06 -0500

    chore(package.json): add mode-watcher package as a dependency feat(+layout.svelte): add ModeWatcher component from mode-watcher package to enable dynamic mode switching feat(+layout.svelte): add Toaster component from svelte-sonner package to display notifications

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,11 +2,17 @@
 	import '../app.pcss';
 
 	import { Bar } from '@bobbymannino/svelte-progress';
+	import { ModeWatcher } from 'mode-watcher';
+	import { Toaster } from 'svelte-sonner';
 
 	import SiteMainHeader from '$components/siteMainHeader/SiteMainHeader.svelte';
 </script>
 
 <Bar color="#6D28D9" size="big" speed="fast" />
+
+<Toaster richColors closeButton position="top-right" />
+
+<ModeWatcher />
 
 <SiteMainHeader />
 
