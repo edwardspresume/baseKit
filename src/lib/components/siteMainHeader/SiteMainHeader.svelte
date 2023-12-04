21c7282c419ce85d6561dee5f5262a8418eb68e8
Edwards Presume <edwardspresume@gmail.com>
2023-12-04 07:06:45 -0500

    Update site main header with button component

--- src/lib/components/siteMainHeader/SiteMainHeader.svelte
+++ src/lib/components/siteMainHeader/SiteMainHeader.svelte
@@ -2,13 +2,14 @@
 	import { route } from '$lib/ROUTES';
 
 	import FeedbackForm from '$components/form/FeedbackForm.svelte';
+	import Button from '$components/ui/button/button.svelte';
 	import SiteMainNav from './SiteMainNav.svelte';
 	import ThemeToggler from './ThemeToggler.svelte';
 </script>
 
 <header class="relative mb-10 border-b">
 	<div class="container flex items-center justify-between p-2">
-		<a href={route('/')} class="text-2xl font-bold">Logo</a>
+		<Button href={route('/')} variant="link" class="text-2xl font-bold">Logo</Button>
 
 		<div class="flex gap-2">
 			<FeedbackForm />
