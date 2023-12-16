53f08d613192da272c783a8956ce9291fbab81df
Edwards Presume <edwardspresume@gmail.com>
2023-12-16 11:34:34 -0500

    Fix async bug in load function

--- src/routes/+layout.server.ts
+++ src/routes/+layout.server.ts
@@ -5,5 +5,5 @@ import { superValidate } from 'sveltekit-superforms/server';
 import { FeedbackValidationSchema } from '$validations/feedbackValidationSchema';
 
 export const load = (async () => {
-	return { feedbackForm: superValidate(FeedbackValidationSchema) };
+	return { feedbackForm: await superValidate(FeedbackValidationSchema) };
 }) satisfies LayoutServerLoad;
