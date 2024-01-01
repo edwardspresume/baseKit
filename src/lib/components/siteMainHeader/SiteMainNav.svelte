bf009f3e612d3766a4150b9c5eea404c682f30e7
Edwards Presume <edwardspresume@gmail.com>
2024-01-01 10:00:29 -0500

    chore(SiteMainNav.svelte): add navigation buttons links for Home and About pages to the main navigation menu for improved user experience

--- src/lib/components/siteMainHeader/SiteMainNav.svelte
+++ src/lib/components/siteMainHeader/SiteMainNav.svelte
@@ -5,8 +5,10 @@
 <script lang="ts">
 	import type { HTMLAttributes } from 'svelte/elements';
 
+	import { route } from '$lib/ROUTES';
 	import { cn } from '$lib/utils/styleTransitionUtils';
 
+	import Button from '$components/ui/button/button.svelte';
 	import SiteMainNavMenuToggleBtn, { isSiteNavMenuOpen } from './SiteMainNavMenuToggleBtn.svelte';
 
 	let className: HTMLAttributes<HTMLElement>['class'] = undefined;
@@ -19,9 +21,18 @@
 	id={MAIN_NAV_ID}
 	aria-label="Main Navigation"
 	class={cn(
-		`absolute z-50 top-full left-0 h-svh w-full px-4 pt-10 transition-transform duration-300 sm:h-full sm:relative sm:w-fit sm:-translate-x-0 sm:px-0  sm:pt-0 bg-secondary sm:bg-transparent ${
+		`absolute left-0 z-50 order-first w-full px-4 pt-10 transition-transform duration-300 top-full h-svh sm:h-full sm:relative sm:w-fit sm:-translate-x-0 sm:p-0 bg-secondary sm:bg-transparent ${
 			!$isSiteNavMenuOpen ? '-translate-x-full' : ''
 		}`,
 		className
 	)}
-></nav>
+>
+	<ul class="grid gap-5 sm:gap-2 sm:flex *:*:w-full">
+		<li>
+			<Button href={route('/')} variant="outline">Home</Button>
+		</li>
+		<li>
+			<Button href={route('/about')} variant="outline">About</Button>
+		</li>
+	</ul>
+</nav>
