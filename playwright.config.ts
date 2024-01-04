c53875614349ee9e9fd24c51307704c82ba69d46
Edwards Presume <edwardspresume@gmail.com>
2024-01-04 16:02:02 -0500

    Update Playwright configuration

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,12 +1,18 @@
 import type { PlaywrightTestConfig } from '@playwright/test';
 
 const config: PlaywrightTestConfig = {
+	testDir: 'tests/e2e',
+	testMatch: /(.+\.)?(test|spec)\.[jt]s/,
+
+	timeout: 15 * 1000,
+	expect: {
+		timeout: 5 * 1000
+	},
+	fullyParallel: true,
 	webServer: {
 		command: 'npm run build && npm run preview',
 		port: 4173
-	},
-	testDir: 'tests',
-	testMatch: /(.+\.)?(test|spec)\.[jt]s/
+	}
 };
 
 export default config;
