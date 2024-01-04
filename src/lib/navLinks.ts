f49551b43087bbdbdecf4c8047be62e6605ea2ae
Edwards Presume <edwardspresume@gmail.com>
2024-01-03 23:57:18 -0500

    refactor(navLinks.ts): remove unnecessary JSDoc comment and update parameter name for better readability

--- src/lib/navLinks.ts
+++ src/lib/navLinks.ts
@@ -9,22 +9,12 @@ export type NavLink = {
 	isExternal?: boolean;
 };
 
-/**
- * Create a NavLink object for use in navigation menus.
- *
- * @param {string} title - Display text for the link.
- * @param {string} href - Route path for navigation
- * @param {string} [ariaLabel=title] - Accessible label for the link; defaults to the title.
- * @param {boolean} [isExternal=false] - Indicates if the link leads outside the app (e.g., to an external site).
- * @returns {NavigationLink} An object representing the navigation link.
- * @throws {Error} Throws an error if title or path is not provided.
- */
 export const createNavLink = (
 	title: string,
 	href: string,
 	ariaLabel: AriaAttributes['aria-label'] = title,
 	isExternal: boolean = false
-): NavLink => {
+) => {
 	if (!title || !href) {
 		throw new Error('Title and href are required to create a NavLink');
 	}
