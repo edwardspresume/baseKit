3ea7ade964600a4303adeb882736a0ad1f19894c
Edwards Presume <edwardspresume@gmail.com>
2024-01-05 18:21:52 -0500

    Add svelte-meta-tags and just-extend dependencies

--- src/app.d.ts
+++ src/app.d.ts
@@ -1,10 +1,14 @@
 // See https://kit.svelte.dev/docs/types#app
 // for information about these interfaces
+
+import type { MetaTagsProps } from 'svelte-meta-tags';
 declare global {
 	namespace App {
 		// interface Error {}
 		// interface Locals {}
-		// interface PageData {}
+		interface PageData {
+			pageMetaTags: MetaTagsProps;
+		}
 		// interface PageState {}
 		// interface Platform {}
 	}
