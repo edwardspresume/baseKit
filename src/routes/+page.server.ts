80d1e34fa6cfbae69d3c4d6668f811e8a3b44211
Edwards Presume <edwardspresume@gmail.com>
2024-01-05 18:52:57 -0500

    Add page meta tags to page server loads

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -1,5 +1,18 @@
 import type { PageServerLoad } from './$types';
 
+import { createPageMetaTags } from '$lib/utils/metaTags';
+
 export const load: PageServerLoad = async () => {
-	return {};
+	const pageMetaTags = createPageMetaTags({
+		title: 'Home page',
+		description: 'Home page description',
+		openGraph: {
+			title: 'Open Graph Title home page',
+			description: 'Open Graph Description home page'
+		}
+	});
+
+	return {
+		pageMetaTags: Object.freeze(pageMetaTags)
+	};
 };
--- src/routes/about/+page.server.ts
+++ src/routes/about/+page.server.ts
@@ -1,5 +1,18 @@
 import type { PageServerLoad } from './$types';
 
+import { createPageMetaTags } from '$lib/utils/metaTags';
+
 export const load = (async () => {
-	return {};
+	const pageMetaTags = createPageMetaTags({
+		title: 'About page',
+		description: 'About page description',
+		openGraph: {
+			title: 'Open Graph Title about page',
+			description: 'Open Graph Description about page'
+		}
+	});
+
+	return {
+		pageMetaTags: Object.freeze(pageMetaTags)
+	};
 }) satisfies PageServerLoad;
