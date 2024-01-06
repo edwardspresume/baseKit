5d01ca9d2794f60ef0122b707f8ebafb93a92b7c
Edwards Presume <edwardspresume@gmail.com>
2024-01-06 17:42:25 -0500

    Refactor mainNavLinks iteration in SiteMainNav.svelte

--- src/lib/components/siteMainHeader/SiteMainNav.svelte
+++ src/lib/components/siteMainHeader/SiteMainNav.svelte
@@ -25,6 +25,8 @@
 	// static classes
 	const staticClasses =
 		'absolute left-0 z-50 w-full px-4 pt-10 transition-transform duration-300 top-full h-svh sm:h-full sm:relative sm:w-fit sm:-translate-x-0 sm:p-0 bg-secondary sm:bg-transparent';
+
+	console.log('mainNavLinks', mainNavLinks);
 </script>
 
 <nav
@@ -33,7 +35,7 @@
 	class={cn(staticClasses, dynamicClasses, className)}
 >
 	<ul class="grid gap-5 *:*:w-full sm:flex sm:gap-2">
-		{#each mainNavLinks as link}
+		{#each Object.values(mainNavLinks) as link}
 			<li>
 				<Button href={link.href} variant="outline" aria-label={link.ariaLabel}>
 					{link.title}
