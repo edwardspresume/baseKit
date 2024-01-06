f85f93eaf41f72910c8cb3ce5f0d06a6e0ef4026
Edwards Presume <edwardspresume@gmail.com>
2024-01-06 15:01:45 -0500

    refactor(page.server.ts): remove unnecessary Open Graph properties from pageMetaTags object as they set using the provided title and description

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -5,11 +5,7 @@ import { createPageMetaTags } from '$lib/utils/metaTags';
 export const load: PageServerLoad = async () => {
 	const pageMetaTags = createPageMetaTags({
 		title: 'Home page',
-		description: 'Home page description',
-		openGraph: {
-			title: 'Open Graph Title home page',
-			description: 'Open Graph Description home page'
-		}
+		description: 'Home page description'
 	});
 
 	return {
--- src/routes/about/+page.server.ts
+++ src/routes/about/+page.server.ts
@@ -5,11 +5,7 @@ import { createPageMetaTags } from '$lib/utils/metaTags';
 export const load = (async () => {
 	const pageMetaTags = createPageMetaTags({
 		title: 'About page',
-		description: 'About page description',
-		openGraph: {
-			title: 'Open Graph Title about page',
-			description: 'Open Graph Description about page'
-		}
+		description: 'About page description'
 	});
 
 	return {
