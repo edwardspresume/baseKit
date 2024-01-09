c697d2d15e14509f9ded111841f81e1d9548794f
Edwards Presume <edwardspresume@gmail.com>
2024-01-09 09:10:43 -0500

    Refactor imageValidationSchema to use file extensions instead of full MIME types

--- src/lib/validations/imageValidationSchema.ts
+++ src/lib/validations/imageValidationSchema.ts
@@ -2,21 +2,15 @@ import { z } from 'zod';
 
 export const MAX_FILE_SIZE_MB = 20;
 
-export const SUPPORTED_IMAGE_TYPES = [
-	'image/jpeg',
-	'image/png',
-	'image/webp',
-	'image/avif',
-	'image/gif'
-];
+export const SUPPORTED_IMAGE_TYPES = ['jpeg', 'png', 'webp', 'avif', 'gif'];
 
 export const IMAGE_VALIDATION_ERROR_MESSAGES = {
-    noFileUploaded: 'Please upload a file.',
-    notAnImage: 'The file you uploaded is not an image.',
-    unsupportedImageType: `The image type you uploaded is not supported. Please upload an image of one of the following types: ${Object.values(
-        SUPPORTED_IMAGE_TYPES
-    ).join(', ')}.`,
-    fileTooLarge: `The file size must be less than ${MAX_FILE_SIZE_MB} MB.`
+	noFileUploaded: 'Please upload a file.',
+	notAnImage: 'The file you uploaded is not an image.',
+	unsupportedImageType: `The image type you uploaded is not supported. Please upload an image of one of the following types: ${SUPPORTED_IMAGE_TYPES.join(
+		', '
+	)}.`,
+	fileTooLarge: `The file size must be less than ${MAX_FILE_SIZE_MB} MB.`
 };
 
 export const imageValidationSchema = z.object({
@@ -24,9 +18,15 @@ export const imageValidationSchema = z.object({
 		.instanceof(File)
 		.refine((file) => file.size > 0, IMAGE_VALIDATION_ERROR_MESSAGES.noFileUploaded)
 		.refine((file) => file.type.startsWith('image/'), IMAGE_VALIDATION_ERROR_MESSAGES.notAnImage)
-		.refine((file) => SUPPORTED_IMAGE_TYPES.includes(file.type), {
-			message: IMAGE_VALIDATION_ERROR_MESSAGES.unsupportedImageType
-		})
+		.refine(
+			(file) => {
+				const fileExtension = file.type.split('/')[1];
+				return fileExtension ? SUPPORTED_IMAGE_TYPES.includes(fileExtension) : false;
+			},
+			{
+				message: IMAGE_VALIDATION_ERROR_MESSAGES.unsupportedImageType
+			}
+		)
 		.refine((file) => file.size <= MAX_FILE_SIZE_MB * 1024 * 1024, {
 			message: IMAGE_VALIDATION_ERROR_MESSAGES.fileTooLarge
 		})
