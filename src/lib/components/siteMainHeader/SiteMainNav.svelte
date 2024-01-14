886ac89ffe15e304f6f8afd12150897b4df534f1
Edwards Presume <edwardspresume@gmail.com>
2024-01-14 18:12:51 -0500

    Remove console.log statements

--- src/lib/components/siteMainHeader/SiteMainNav.svelte
+++ src/lib/components/siteMainHeader/SiteMainNav.svelte
@@ -26,7 +26,6 @@
 	const staticClasses =
 		'absolute left-0 z-50 w-full px-4 pt-10 transition-transform duration-300 top-full h-svh sm:h-full sm:relative sm:w-fit sm:-translate-x-0 sm:p-0 bg-secondary sm:bg-transparent';
 
-	console.log('mainNavLinks', mainNavLinks);
 </script>
 
 <nav
--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -2,8 +2,6 @@
 	import type { PageData } from './$types';
 
 	export let data: PageData;
-
-	console.log(data);
 </script>
 
 <h1 class="text-2xl font-bold">About page</h1>
