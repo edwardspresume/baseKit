6a1817f35fd0c2a45359bc44f5c63818a3df6786
Edwards Presume <edwardspresume@gmail.com>
2024-01-15 13:25:50 -0500

    feat(SiteMainNav.svelte): add logic to highlight current page in navigation menu for better user experience

--- src/lib/components/siteMainHeader/SiteMainNav.svelte
+++ src/lib/components/siteMainHeader/SiteMainNav.svelte
@@ -7,6 +7,7 @@
 </script>
 
 <script lang="ts">
+	import { page } from '$app/stores';
 	import type { HTMLAttributes } from 'svelte/elements';
 
 	import { mainNavLinks } from '$lib/navLinks';
@@ -25,7 +26,6 @@
 	// static classes
 	const staticClasses =
 		'absolute left-0 z-50 w-full px-4 pt-10 transition-transform duration-300 top-full h-svh sm:h-full sm:relative sm:w-fit sm:-translate-x-0 sm:p-0 bg-secondary sm:bg-transparent';
-
 </script>
 
 <nav
@@ -35,8 +35,16 @@
 >
 	<ul class="grid gap-5 *:*:w-full sm:flex sm:gap-2">
 		{#each Object.values(mainNavLinks) as link}
+			{@const isCurrentPage = $page.url.pathname === link.href ? 'page' : undefined}
+
 			<li>
-				<Button href={link.href} variant="outline" aria-label={link.ariaLabel}>
+				<Button
+					href={link.href}
+					variant="outline"
+					aria-label={link.ariaLabel}
+					aria-current={isCurrentPage}
+					class={isCurrentPage ? 'text-primary-foreground' : ''}
+				>
 					{link.title}
 				</Button>
 			</li>
