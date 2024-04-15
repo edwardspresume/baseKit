e16624819a122c65c909c7c956d0fbb4af59bf79
Edwards Presume <edwardspresume@gmail.com>
2024-04-15 09:09:09 -0400

    Refactor siteMainNavMenuToggleBtn component

--- src/lib/components/siteMainHeader/SiteMainNavMenuToggleBtn.svelte
+++ src/lib/components/siteMainHeader/SiteMainNavMenuToggleBtn.svelte
@@ -25,16 +25,16 @@
 	aria-expanded={$isSiteNavMenuOpen}
 	aria-label="Toggle Main Navigation"
 	on:click={() => ($isSiteNavMenuOpen = !$isSiteNavMenuOpen)}
-	class="rounded border p-2 sm:hidden"
+	class="p-2 border rounded sm:hidden"
 >
-	<div />
-	<div />
-	<div />
+	<span />
+	<span />
+	<span />
 </button>
 
 <style lang="postcss">
-	button div {
-		@apply h-[0.1875rem] w-7 rounded bg-foreground transition-transform duration-300 ease-in-out;
+	button span {
+		@apply block h-[0.1875rem] w-7 rounded bg-foreground transition-transform duration-300 ease-in-out;
 
 		&:not(:last-of-type) {
 			margin-block-end: 0.375rem;
@@ -42,15 +42,15 @@
 	}
 
 	button[aria-expanded='true'] {
-		div:nth-child(2) {
+		span:nth-child(2) {
 			opacity: 0;
 		}
 
-		div:nth-child(1) {
+		span:nth-child(1) {
 			transform: translateY(0.5625rem) rotate(45deg);
 		}
 
-		div:nth-child(3) {
+		span:nth-child(3) {
 			transform: translateY(-0.5625rem) rotate(-45deg);
 		}
 	}
