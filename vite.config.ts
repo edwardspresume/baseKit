25b69d3f8218dd7b343a4201d24e469fbaa08c5a
Edwards Presume <edwardspresume@gmail.com>
2024-05-17 20:20:16 -0400

    chore: Update Vite configuration to use latest dependencies

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
 import { enhancedImages } from '@sveltejs/enhanced-img';
 import { sveltekit } from '@sveltejs/kit/vite';
+import { defineConfig } from 'vite';
 import { kitRoutes } from 'vite-plugin-kit-routes';
-import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
 	plugins: [enhancedImages(), sveltekit(), kitRoutes()]
