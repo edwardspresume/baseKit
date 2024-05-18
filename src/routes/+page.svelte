df1b7bf23a8134824aa0b6c1a7f7f1f5d5ce6180
Edwards Presume <edwardspresume@gmail.com>
2024-05-17 21:26:28 -0400

    refactor: Remove unused script tag in +page.svelte

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,11 +1,7 @@
 <script lang="ts">
-	import type { PageData } from './$types';
-
 	import { toast } from 'svelte-sonner';
 
 	import Button from '$components/ui/button/button.svelte';
-
-	export let data: PageData;
 </script>
 
 <h1 class="text-2xl font-bold">Home page</h1>
