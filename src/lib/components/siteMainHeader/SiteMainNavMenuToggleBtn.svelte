b62d1fe2d79d1958ee631d03bb92586501b32003
Edwards Presume <edwardspresume@gmail.com>
2024-06-22 21:00:59 -0400

    - Combine effects into a single reactive statement for better code organization. - Use CSS custom properties for line height and spacing to make the style more maintainable.
    - Update the hamburger menu animation to use CSS custom properties for consistency and easier future adjustments.

--- src/lib/components/siteMainHeader/SiteMainNavMenuToggleBtn.svelte
+++ src/lib/components/siteMainHeader/SiteMainNavMenuToggleBtn.svelte
@@ -1,15 +1,12 @@
 <script>
 	import { navigating } from '$app/stores';
-
 	import { MAIN_NAV_ID, navigationMenuState } from './SiteMainNav.svelte';
 
-	// Close the site nav menu when navigating
 	$effect(() => {
+		// Close the site nav menu when navigating
 		if ($navigating) navigationMenuState.isOpen = false;
-	});
 
-	// Prevent scrolling when site nav menu is open
-	$effect(() => {
+		// Prevent scrolling when site nav menu is open
 		document.body.style.overflow = navigationMenuState.isOpen ? 'hidden' : 'auto';
 	});
 </script>
@@ -19,7 +16,7 @@
 	aria-expanded={navigationMenuState.isOpen}
 	aria-label="Toggle Main Navigation"
 	onclick={() => (navigationMenuState.isOpen = !navigationMenuState.isOpen)}
-	class="p-2 border rounded sm:hidden"
+	class="rounded border p-2 sm:hidden"
 >
 	<span></span>
 	<span></span>
@@ -28,24 +25,28 @@
 
 <style lang="postcss">
 	button span {
-		@apply block h-[0.1875rem] w-7 rounded bg-foreground transition-transform duration-300 ease-in-out;
+		--line-height: 0.187rem;
+		--line-spacing: 0.41rem;
 
-		&:not(:last-of-type) {
-			margin-block-end: 0.375rem;
-		}
+		@apply block w-6 rounded bg-foreground transition-all duration-300 ease-in-out;
+		block-size: var(--line-height);
+	}
+
+	button span:not(:last-of-type) {
+		margin-block-end: var(--line-spacing);
 	}
 
 	button[aria-expanded='true'] {
+		span:nth-child(1) {
+			transform: translateY(calc(var(--line-height) + var(--line-spacing))) rotate(45deg);
+		}
+
 		span:nth-child(2) {
 			opacity: 0;
 		}
 
-		span:nth-child(1) {
-			transform: translateY(0.5625rem) rotate(45deg);
-		}
-
 		span:nth-child(3) {
-			transform: translateY(-0.5625rem) rotate(-45deg);
+			transform: translateY(calc(-1 * (var(--line-height) + var(--line-spacing)))) rotate(-45deg);
 		}
 	}
 </style>
